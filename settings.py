b757632b20255900a9cf85a00e84108c0ec73640
Dmitry <sdvkam@gmail.com>
2022-06-06 12:12:06 +1200

    Change settings: SQLALCHEMY_DATABASE_URI

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ set_symbols = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
